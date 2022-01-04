ce876596e50bdfba468ef6afc456a3d46ca4ac5a
Vahtee Boo <ronnie.kaa@bigmir.net>
2022-01-04 09:30:07 +0200

    part 3 of 11

--- Project7p2of11/ContentView.swift
+++ Project7p2of11/ContentView.swift
@@ -7,10 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SecondView: View {
+    
+    let name: String
+    
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Second View for you, \(name)!")
+            Button("Dismiss") {
+                dismiss()
+            }
+        }
+    }
+}
+
+struct ContentView: View {
+    
+    @State private var showingSheet = false
+    var body: some View {
+        
+        Button("Show Second Sheet") {
+            showingSheet.toggle()
+        }
+        .sheet(isPresented: $showingSheet) {
+            SecondView(name: "Vahtang")
+        }
+        
     }
 }
 
